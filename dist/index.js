a55da8fe75168b940bc82d7394a8467dc6f1963d
IsraCoder <IZughyer@gmail.com>
2023-09-02 10:24:55 +0300

    fourth

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 var app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Server UP!");
