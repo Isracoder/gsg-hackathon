13e46dd4bf44625125bee6e2942595c3795e79fa
IsraCoder <IZughyer@gmail.com>
2023-09-02 10:15:28 +0300

    second

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+var app = express();
+const PORT = 3000;
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Server UP!");
+});
+app.use((req, res) => {
+    res.status(404).send("You requested something I don't have :(");
+});
+app.listen(PORT, () => {
+    console.log(`App is running and Listening on port ${PORT}`);
+    //   db.initialize();
+});
